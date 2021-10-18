2384e3814b2fa4b414b030f857aabdbc4fd14644
flo <flori.sonstwas@outlook.de>
2021-10-18 22:07:57 +0200

    Pin Definitions

---
+++ MainESP/include/pin_definitions.h
@@ -0,0 +1,31 @@
+#ifndef PIN_DEFINITIONS
+#define PIN_DEFINITIONS
+
+#define PIN_SENS1 33
+#define PIN_SENS2 34
+#define PIN_SENS3 35
+#define PIN_SENS4 2
+
+#define PIN_BUZZ1 26
+#define PIN_BUZZ2 27
+
+#define PIN_SERVO1 16
+#define PIN_SERVO2 17
+#define PIN_SERVO3 25
+
+#define PIN_SDA 18
+#define PIN_SCL 19
+
+#define PIN_RX_CAM 21 // TX of Cam, RX of EXP
+#define PIN_TX_CAM 22 // RX of Cam, TX of EXP
+
+#define PIN_DIP1 4
+#define PIN_DIP2 30
+
+#define PIN_BATPROBE 32
+
+#define PIN_ROT_IRQ 5
+
+#define LED_BUILTIN 2
+
+#endif // PIN_DEFINITIONS
