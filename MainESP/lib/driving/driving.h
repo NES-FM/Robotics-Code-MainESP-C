e5c0a88064a3d5e8c9c3415b568d27aeb22697db
flo <flori.sonstwas@outlook.de>
2021-12-12 18:18:03 +0100

    code cleanup

--- MainESP/lib/driving/driving.h
+++ MainESP/lib/driving/driving.h
@@ -22,20 +22,6 @@ bool driving_interesting_actual_ltype_override = false;
 void drive_sensor_array()
 {
     // Line is left...
-    // if (2 < CUART_array_left_sensor && CUART_array_left_sensor < 6 && CUART_array_right_sensor < 2)
-    // {
-    //     // ... and not in the middle...
-    //     if (CUART_array_mid_sensor <= 2)
-    //     {
-    //         // ... but not too far left
-    //         if (CUART_sensor_array[5] == 0)
-    //             drive(DRIVE_SPEED_NORMAL,DRIVE_SPEED_HIGHER);
-    //         // ... and far left
-    //         else
-    //             drive(-DRIVE_SPEED_HALF,DRIVE_SPEED_NORMAL);
-    //     }
-    // }
-
     if (!driving_interesting_situation && CUART_array_left_sensor >= 1 && CUART_array_right_sensor == 0)
     {
         if ((CUART_sensor_array[3] == true || CUART_sensor_array[4] == true) && CUART_array_left_sensor > 3)
@@ -53,20 +39,6 @@ void drive_sensor_array()
     }
 
     // Line is right...
-    // if (2 < CUART_array_right_sensor && CUART_array_right_sensor < 6 && CUART_array_left_sensor < 2)
-    // {
-    //     // ... and not in the middle...
-    //     if (CUART_array_mid_sensor <= 2)
-    //     {
-    //         // ... but not too far right
-    //         if (CUART_sensor_array[20] == 0)
-    //             drive(DRIVE_SPEED_HIGHER,DRIVE_SPEED_NORMAL);
-    //         // ... and far right
-    //         else
-    //             drive(DRIVE_SPEED_NORMAL,-DRIVE_SPEED_HALF);
-    //     }
-    // }
-
     if (!driving_interesting_situation && CUART_array_left_sensor == 0 && CUART_array_right_sensor >= 1)
     {
         if ((CUART_sensor_array[22] == true || CUART_sensor_array[21] == true) && CUART_array_right_sensor > 3)
@@ -169,11 +141,6 @@ void drive_sensor_array()
         }
 
         // Serial.printf("Driving interesting situation with ltype: %d, Bias: L: %s, R: %s, B: %s\r\n", driving_interesting_actual_ltype, driving_interesting_bias_left ? "T" : "F", driving_interesting_bias_right ? "T" : "F", driving_interesting_bias_both ? "T" : "F");
-        // driving_interesting_situation_locked = false;
-        // driving_interesting_situation = false;
-        // driving_interesting_bias_left = false;
-        // driving_interesting_bias_right = false;
-        // driving_interesting_bias_both = false;
 
         // Actual driving
 
@@ -183,6 +150,7 @@ void drive_sensor_array()
                 driving_interesting_actual_ltype == CUART_LTYPE_t || 
                 driving_interesting_actual_ltype == CUART_LTYPE_X) && CUART_green_dots[2]))
         {
+            Serial.printf("Interesting: Turning left with ltype %d and dl green dot %s\r\n", driving_interesting_actual_ltype, CUART_green_dots[2] ? "True" : "False");
             drive(-DRIVE_SPEED_NORMAL, DRIVE_SPEED_NORMAL);
             while(CUART_array_mid_sensor > 2) {
                 display.tick();
@@ -198,7 +166,6 @@ void drive_sensor_array()
                 vTaskDelay( pdMS_TO_TICKS( 10 ) );
             }
             drive(DRIVE_SPEED_NORMAL, DRIVE_SPEED_NORMAL);
-            Serial.printf("Interesting: Turning left with ltype %d and dl green dot %s\r\n", driving_interesting_actual_ltype, CUART_green_dots[2] ? "True" : "False");
         }
         // Turn right
         else if ((driving_interesting_actual_ltype == CUART_LTYPE_90r) || 
@@ -206,6 +173,7 @@ void drive_sensor_array()
                     driving_interesting_actual_ltype == CUART_LTYPE_t || 
                     driving_interesting_actual_ltype == CUART_LTYPE_X) && CUART_green_dots[3]))
         {
+            Serial.printf("Interesting: Turning right with ltype %d and dr green dot %s\r\n", driving_interesting_actual_ltype, CUART_green_dots[3] ? "True" : "False");
             drive(DRIVE_SPEED_NORMAL, -DRIVE_SPEED_NORMAL);
             while(CUART_array_mid_sensor > 2) {
                 display.tick();
@@ -221,13 +189,12 @@ void drive_sensor_array()
                 vTaskDelay( pdMS_TO_TICKS( 10 ) );
             }
             drive(DRIVE_SPEED_NORMAL, DRIVE_SPEED_NORMAL);
-            Serial.printf("Interesting: Turning right with ltype %d and dr green dot %s\r\n", driving_interesting_actual_ltype, CUART_green_dots[3] ? "True" : "False");
         }
         // Keep Straight
         else
         {
-            drive(DRIVE_SPEED_NORMAL, DRIVE_SPEED_NORMAL);
             Serial.printf("Interesting: Keeping Straight with ltype %d and dl green dot %s and dr green dot %s\r\n", driving_interesting_actual_ltype, CUART_green_dots[2] ? "True" : "False", CUART_green_dots[3] ? "True" : "False");
+            drive(DRIVE_SPEED_NORMAL, DRIVE_SPEED_NORMAL);
         }
 
         driving_interesting_situation = false;
