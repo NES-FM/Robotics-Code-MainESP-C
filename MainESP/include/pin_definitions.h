6301f471607073070cd39ba466a1962837800754
NES-FM <flori.sonstwas@outlook.de>
2022-03-25 14:55:57 +0100

    new pcb pindef

--- MainESP/include/pin_definitions.h
+++ MainESP/include/pin_definitions.h
@@ -4,11 +4,10 @@
 #define PIN_SENS1 33
 #define PIN_SENS2 34
 #define PIN_SENS3 35
-#define PIN_SENS4 2
+#define PIN_SENS4 4
 // New PCB: #define PIN_SENS4 4
 
 #define PIN_BUZZ1 26
-#define PIN_BUZZ2 27
 
 #define PIN_SERVO1 16
 #define PIN_SERVO2 17
@@ -20,7 +19,7 @@
 #define PIN_RX_CAM 21 // TX of Cam, RX of EXP
 #define PIN_TX_CAM 22 // RX of Cam, TX of EXP
 
-#define PIN_DIP1 4
+#define PIN_DIP1 15
 #define PIN_DIP2 23
 #define PIN_DIP_WETTKAMPFMODUS 27
 
@@ -28,6 +27,6 @@
 
 #define PIN_ROT_IRQ 5
 
-// #define LED_BUILTIN 2
+#define LED_BUILTIN 2
 
 #endif // PIN_DEFINITIONS
