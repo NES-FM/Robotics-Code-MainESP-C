876d13fb2933ee859189865230350cbb3545f8e2
NES-FM <flori.sonstwas@outlook.de>
2022-05-02 22:09:30 +0200

    Time Of Flight sensor

---
+++ MainESP/lib/tof/tof.cpp
@@ -0,0 +1,199 @@
+#include "tof.h"
+
+void tof::init()
+{
+    if (_xshut != -1) 
+    {
+        pinMode(_xshut, OUTPUT);
+        digitalWrite(_xshut, LOW);
+    }
+}
+
+void tof::begin(uint8_t address)
+{
+    if (_enabled)
+    {
+        sensor->setTimeout(500);
+        if (!sensor->init())
+        {
+            Serial.println(F("Failed to detect and initialize VL53L0X!"));
+            this->enable(false);
+            _error = TOF_ERROR_FAILED_TO_INITIALIZE;
+            return;
+        }
+        delay(10);
+        changeAddress(address);
+        _error = TOF_ERROR_NONE;
+    }
+}
+
+void tof::enable(bool enabled) {
+    _enabled = enabled; 
+    if (!enabled)
+        _error = TOF_ERROR_NOT_ENABLED; 
+}
+
+void tof::setLongRangeMode(bool mode)
+{
+    if (_enabled)
+    {
+        _long_range = mode;
+        if (mode)
+        {
+            // lower the return signal rate limit (default is 0.25 MCPS)
+            sensor->setSignalRateLimit(0.1);
+            // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+            sensor->setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+            sensor->setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+        }
+        else
+        {
+            // lower the return signal rate limit (default is 0.25 MCPS)
+            sensor->setSignalRateLimit(0.25);
+            // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+            sensor->setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 14);
+            sensor->setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 10);
+        }
+    }
+}
+
+void tof::setHighSpeed(bool mode)
+{
+    if (_enabled)
+    {
+        if (mode)
+        {
+            _high_accuracy = false;
+            _high_speed = true;
+            sensor->setMeasurementTimingBudget(20000);
+        }
+        else
+        {
+            _high_accuracy = false;
+            _high_speed = false;
+            sensor->setMeasurementTimingBudget(33000);
+        }
+    }
+}
+
+void tof::setHighAccuracy(bool mode)
+{
+    if (_enabled)
+    {
+        if (mode)
+        {
+            _high_accuracy = true;
+            _high_speed = false;
+            sensor->setMeasurementTimingBudget(200000);
+        }
+        else
+        {
+            _high_accuracy = false;
+            _high_speed = false;
+            sensor->setMeasurementTimingBudget(33000);
+        }
+    }
+}
+
+void tof::setContinuous(bool mode, uint32_t period_ms)
+{
+    if (_enabled)
+    {
+        continuous_mode = mode;
+        if (mode)
+        {
+            sensor->startContinuous(period_ms);
+        }
+        else
+        {
+            sensor->stopContinuous();
+        }
+    }
+}
+
+uint16_t tof::getMeasurement()
+{
+    if (_enabled)
+    {
+        uint16_t measurment;
+        if (continuous_mode)
+        {
+            measurment = sensor->readRangeContinuousMillimeters();
+        }
+        else
+        {
+            measurment = sensor->readRangeSingleMillimeters();
+        }
+        last_measurement = measurment;
+
+        if (sensor->timeoutOccurred())
+        {
+            _error = TOF_ERROR_TIMEOUT;
+        }
+        else if (measurment > 8000)
+        {
+            _error = TOF_ERROR_MAX_DISTANCE;
+        }
+        else
+        {
+            _error = TOF_ERROR_NONE;
+        }
+
+        return measurment;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void tof::holdReset()
+{
+    if (_xshut != -1)
+    {
+        digitalWrite(_xshut, LOW);
+    }
+}
+
+void tof::releaseReset()
+{
+    if (_xshut != -1)
+    {
+        digitalWrite(_xshut, HIGH);
+    }
+}
+
+void tof::reset()
+{
+    if (_xshut != -1)
+    {
+        this->holdReset();
+        delay(10);
+        this->releaseReset();
+    }
+}
+
+void tof::changeAddress(uint8_t address)
+{
+    if (_enabled)
+        sensor->setAddress(address);
+}
+
+String tof::getMeasurementErrorString()
+{
+    switch (_error)
+    {
+        case TOF_ERROR_NONE:
+            return F("None");
+        case TOF_ERROR_NOT_ENABLED:
+            return F("Not enabled");
+        case TOF_ERROR_TIMEOUT:
+            return F("Timeout");
+        case TOF_ERROR_MAX_DISTANCE:
+            return F("Max distance exceeded");
+        case TOF_ERROR_FAILED_TO_INITIALIZE:
+            return F("Failed to initialize");
+    }
+    return F("Unknown error");
+}
+
---
+++ MainESP/lib/tof/tof.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "VL53L0X.h"
+
+#define TOF_MAX_VALID_DISTANCE 
+
+class tof {
+    public:
+        enum tof_error_types {
+            TOF_ERROR_NONE,
+            TOF_ERROR_TIMEOUT,
+            TOF_ERROR_MAX_DISTANCE,
+            TOF_ERROR_NOT_ENABLED,
+            TOF_ERROR_FAILED_TO_INITIALIZE
+        };
+
+        tof(uint8_t xshut = -1) { _xshut = xshut; };
+        void init();
+        void begin(uint8_t address = 0b0101001);
+        void enable(bool enabled);
+        void setLongRangeMode(bool mode);
+        void setHighAccuracy(bool mode);
+        void setHighSpeed(bool mode);
+        void setContinuous(bool mode, uint32_t period_ms = 0);
+        inline bool timeoutOccurred() { return sensor->timeoutOccurred(); }
+
+        tof_error_types getMeasurementError() { return _error; };
+        String getMeasurementErrorString();
+
+        void holdReset();
+        void releaseReset();
+        void reset();
+
+        void changeAddress(uint8_t address);
+
+        uint16_t getMeasurement();
+    private:
+        VL53L0X* sensor = new VL53L0X();
+
+        bool _long_range = false;
+        bool _high_accuracy = false;
+        bool _high_speed = false;
+
+        bool continuous_mode = false;
+
+        uint16_t last_measurement = 0;
+
+        uint8_t _xshut = -1;
+
+        bool _enabled = false;
+
+        tof_error_types _error = TOF_ERROR_NOT_ENABLED;
+};
